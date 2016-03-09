cec0d2be4e1ed2eaf8b3159817b3f329c8150cb6
Chris Johns <chrisj@rtems.org>
2016-03-09 15:36:32 +1100

    Update the POSIX user manual version number.

--- posix_users/conf.py
+++ posix_users/conf.py
@@ -4,9 +4,8 @@ sys.path.append(os.path.abspath('../common/'))
 from conf import *
 
 version = '1.0'
-release = '5.0'
+release = '4.11.0'
 
 latex_documents = [
-	('index', 'posix_users.tex', u'RTEMS POSIX Users Documentation', u'RTEMS Documentation Project', 'manual'),
+	('index', 'posix_users.tex', u'RTEMS POSIX API User\'s Guide', u'RTEMS Documentation Project', 'manual'),
 ]
-
