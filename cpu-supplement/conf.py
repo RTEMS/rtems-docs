f7dbf17028db3033b9487815efdfd5b60baada09
Chris Johns <chrisj@rtems.org>
2016-11-09 16:53:20 +1100

    cpu-supplement: Fix the project label.

--- cpu-supplement/conf.py
+++ cpu-supplement/conf.py
@@ -6,6 +6,8 @@ from conf import *
 version = '1.0'
 release = '4.11.0'
 
+project = "RTEMS CPU Supplement Documentation"
+
 latex_documents = [
 	('index', 'cpu-supplement.tex', u'RTEMS CPU Supplement Documentation', u'RTEMS Documentation Project', 'manual'),
 ]
